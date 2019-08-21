fbffa796df5a6e510c574102f94842df2404c5bf
Thart310 <40911634+Thart310@users.noreply.github.com>
2019-08-21 14:50:27 +0700

    test

--- application2/resources/views/welcome.blade.php
+++ application2/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Field Operation System</title>
+        <title>Field Operation Systems</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
